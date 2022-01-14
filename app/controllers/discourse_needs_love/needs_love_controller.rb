cc254a5b855e20c599d5be6ef47d3423cee11290
Blake Erickson <o.blakeerickson@gmail.com>
2022-01-14 15:45:01 -0700

    bypass guardian so that regular users can add the needs-love tag when hidden by a tag group

--- app/controllers/discourse_needs_love/needs_love_controller.rb
+++ app/controllers/discourse_needs_love/needs_love_controller.rb
@@ -6,15 +6,27 @@ module DiscourseNeedsLove
     before_action :ensure_logged_in
 
     def needs_love
-      tag = "needs-love" # Load from plugin site setting
+      tag_name = "needs-love" # Load from plugin site setting
+
       topic = Topic.find_by(id: params[:topic_id])
-      tags = (topic.tags.pluck(:name) + [tag]).uniq
-      results = PostRevisor.new(topic.first_post, topic).revise!(current_user, { tags: tags }, validate_post: false)
-      if results == true
-        render json: success_json
-      else
-        render json: failed_json
+      tag = Tag.find_by(name: tag_name)
+      unless tag
+        tag = Tag.create(name: name)
       end
+
+      old_tag_names = topic.tags.pluck(:name) || []
+      tag_names = ([tag_name] + old_tag_names).uniq
+      tags = Tag.where(name: tag_names)
+
+      topic.tags = tags
+
+      topic.tags_changed = true
+      DiscourseEvent.trigger(
+        :topic_tags_changed,
+        topic, old_tag_names: old_tag_names, new_tag_names: [tag_name]
+      )
+
+      render json: success_json
     end
   end
 end
