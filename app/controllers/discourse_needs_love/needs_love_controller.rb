ffd2aa761b5923b1798ca581972afa6227aa2b3b
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2026-06-15 18:40:00 -0300

    FIX: Needs Love can tag topics the caller cannot see (#60)

--- app/controllers/discourse_needs_love/needs_love_controller.rb
+++ app/controllers/discourse_needs_love/needs_love_controller.rb
@@ -9,15 +9,14 @@ module DiscourseNeedsLove
     before_action :ensure_can_needs_love
 
     def needs_love
-      tag_name = SiteSetting.needs_love_tag
-
       topic = Topic.find_by(id: params[:topic_id])
-      tag = Tag.find_by(name: tag_name)
-      tag = Tag.create(name: tag_name) unless tag
+      guardian.ensure_can_see!(topic)
+
+      tag_name = SiteSetting.needs_love_tag
+      Tag.find_or_create_by(name: tag_name)
 
       old_tag_names = topic.tags.pluck(:name) || []
       tag_names = ([tag_name] + old_tag_names).uniq
-      tags = Tag.where(name: tag_names)
 
       PostRevisor.new(topic.first_post, topic).revise!(
         Discourse.system_user,
